947f343889de2606dd472e415db1c09ceff9371c
phijopho <joshi.spiegel@gmail.com>
2023-11-20 20:15:44 +0100

    added font link to index.php and style.css

--- index.php
+++ index.php
@@ -3,6 +3,10 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Einbinden der Schriftart -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@500&display=swap" rel="stylesheet">
     <!-- Einbinden der style.css -->
     <link rel="stylesheet" href="css/style.css">
     <title>SWIFT rentals</title>
