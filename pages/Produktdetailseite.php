edc53d4fd82630fb69b13a0f3045fa8b41f346a2
clawo <131432761+clawo@users.noreply.github.com>
2023-11-22 11:41:58 +0100

    try1

--- pages/Produktdetailseite.php
+++ pages/Produktdetailseite.php
@@ -7,19 +7,22 @@
      <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet"> 
-    <title>Unsere Flotte</title> 
     <!-- Einbinden der style.css -->
-    <link rel="stylesheet" href="../css/style.css">
-    <link rel="stylesheet" href="../css/styleProduktdetailseite.css">
+    <link rel="stylesheet" href="/css/style.css">
+    <link rel="stylesheet" href="/css/styleProduktdetailseite.css">
+    <title>Unsere Flotte</title> 
 </head>
  
 <body>
 <?php
-include('../includes/header.html'); // Einbindung des Headers
+include('/includes/header.html'); // Einbindung des Headers
+
+include('/includes/footer.html'); // Einbindung des Footers
 ?>
-<?php
-include('../includes/footer.html'); // Einbindung des Footers
-?>
+
+<div>
+    <h1>Hallo</h1>
+</div>
 </body>
  
 </html>
