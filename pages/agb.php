c20729e4c38ab5d796b915a073e33cdab9ed6d2e
philippr1212 <Philipp.Rinau@myhsba.de>
2023-11-23 15:29:07 +0100

    ready to test the sites Datenschutz, AGB and Impressum

--- pages/agb.php
+++ pages/agb.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <!-- Einbinden der style.css -->
+    <!-- Including the style.css -->
     <link rel="stylesheet" href="../css/style.css"> 
     <link rel="stylesheet" href="../css/styleAGB.css">
     <title>SWIFT rentals</title>
@@ -14,82 +14,87 @@
 
 <?php
 
-include('../includes/header.html'); // Einbinden des Headers
+include('../includes/header.html'); // Including the header
 ?>
 
-<div class="AGBContainer"> <!--Klasse um Text im Body zu positionieren -->
-    <h1 class="AGBUeberschrift"> Allgemeine Geschäftsbedingungen für SWIFT rentals </h1>
+<div class="AGBContainer"> <!--Class to position text in the body -->
+    <h1 class="AGBUeberschrift"> Allgemeine Geschäftsbedingungen </h1>
+    <div class="blocksatz">
+        <b>1. Vertragsgegenstand </b> <br>
 
-    <b>1. Vertragsgegenstand </b> <br>
+        <div class="AGBtext"> 
+        Diese Allgemeinen Geschäftsbedingungen regeln die Vermietung von Fahrzeugen durch SWIFT rentals.
+        </div>
 
-    <div class="AGBtext"> 
-    Diese Allgemeinen Geschäftsbedingungen regeln die Vermietung von Fahrzeugen durch SWIFT rentals.
+        <br> <!--To add paragraph-->
+
+
+        <b>2. Anmietung und Fahrzeugrückgabe </b> <br>
+
+        <div class="AGBtext">
+        2.1. Der Mieter erklärt sich damit einverstanden, das Fahrzeug in gutem Zustand zu übernehmen und in dem gleichen Zustand zurückzugeben, wie es ihm übergeben wurde. <br>
+        2.2. Die Mietdauer beginnt und endet gemäß den im Mietvertrag festgelegten Bedingungen. <br>
+        2.3. Bei verspäteter Rückgabe des Fahrzeugs kann eine zusätzliche Gebühr anfallen. <br>
+        </div>
+
+        <br> <!--To add paragraph-->
+
+        <b> 3. Fahrzeugnutzung </b> <br>
+
+        <div class="AGBtext">
+        3.1. Das Fahrzeug darf nur vom Mieter oder den im Mietvertrag genannten Fahrern geführt werden. <br>
+        3.2. Der Mieter verpflichtet sich, das Fahrzeug in Übereinstimmung mit den geltenden Verkehrsregeln und Gesetzen zu verwenden. <br>
+        </div>
+
+        <br> <!--To add paragraph-->
+
+        <b> 4. Mietpreis und Zahlungsbedingungen </b> <br>
+
+        <div class="AGBtext">
+        4.1. Der Mietpreis besteht aus der vereinbarten Tagespauschale inklusive Mehrwertsteuer. <br>
+        4.2. Die Zahlung erfolgt im Voraus. Zusätzliche Kosten werden nach Rückgabe des Fahrzeugs berechnet. <br>
+        </div>
+
+        <br> <!--To add paragraph-->
+
+        <b> 5. Haftung und Versicherung </b> <br>
+
+        <div class="AGBtext">
+        5.1. Der Mieter haftet für Schäden am Fahrzeug während der Mietdauer, es sei denn, der Schaden beruht nicht auf einem Verschulden des Mieters. <br>
+        5.2. Die Haftung des Mieters ist auf den im Mietvertrag festgelegten Selbstbehalt begrenzt. <br>
+        5.3. Das Fahrzeug ist vollkaskoversichert. Schäden müssen jedoch unverzüglich gemeldet werden. <br>
+        </div>
+
+        <br> <!--To add paragraph-->
+
+        <b>6. Stornierung und Änderungen </b> <br>
+
+        <div class="AGBtext">
+        6.1. Stornierungen sind gemäß den im Mietvertrag festgelegten Bedingungen möglich. <br>
+        6.2. Änderungen am Mietvertrag bedürfen der schriftlichen Zustimmung beider Vertragsparteien. <br>
+        </div>
+
+        <br> <!--To add paragraph-->
+
+        <b> 7. Datenschutz </b> <br>
+
+        <div class="AGBtext">
+        Personenbezogene Daten werden gemäß unserer <a href="http://localhost/Autovermietung/pages/datenschutz.php">Datenschutzerklärung</a> behandelt. <br> <!--href um über den Hyperlink zu Datenschutz zu gelangen-->
+        </div>
+
+        <br> <!--To add paragraph-->
+
+        <b> 8. Schlussbestimmungen </b> <br>
+
+        <div class="AGBtext">
+        8.1. Es gilt das Recht der Bundesrepublik Deutschland. <br>
+        8.2. Erfüllungsort und Gerichtsstand ist Hamburg. </p>
+        </div>
     </div>
-    <p> </p> <!--Um Absatz hinzuzufügen-->
-
-
-    <b>2. Anmietung und Fahrzeugrückgabe </b> <br>
-
-    <div class="AGBtext">
-    2.1. Der Mieter erklärt sich damit einverstanden, das Fahrzeug in gutem Zustand zu übernehmen und in dem gleichen Zustand zurückzugeben, wie es ihm übergeben wurde. <br>
-    2.2. Die Mietdauer beginnt und endet gemäß den im Mietvertrag festgelegten Bedingungen. <br>
-    2.3. Bei verspäteter Rückgabe des Fahrzeugs kann eine zusätzliche Gebühr anfallen. <br>
-    </div>
-    <p> </p>
-
-    <b> 3. Fahrzeugnutzung </b> <br>
-
-    <div class="AGBtext">
-    3.1. Das Fahrzeug darf nur vom Mieter oder den im Mietvertrag genannten Fahrern geführt werden. <br>
-    3.2. Der Mieter verpflichtet sich, das Fahrzeug in Übereinstimmung mit den geltenden Verkehrsregeln und Gesetzen zu verwenden. <br>
-    </div>
-    <p> </p>
-
-    <b> 4. Mietpreis und Zahlungsbedingungen </b> <br>
-
-    <div class="AGBtext">
-    4.1. Der Mietpreis besteht aus der vereinbarten Tagespauschale inklusive Mehrwertsteuer. <br>
-    4.2. Die Zahlung erfolgt im Voraus. Zusätzliche Kosten werden nach Rückgabe des Fahrzeugs berechnet. <br>
-    </div>
-    <p> </p>
-
-    <b> 5. Haftung und Versicherung </b> <br>
-
-    <div class="AGBtext">
-    5.1. Der Mieter haftet für Schäden am Fahrzeug während der Mietdauer, es sei denn, der Schaden beruht nicht auf einem Verschulden des Mieters. <br>
-    5.2. Die Haftung des Mieters ist auf den im Mietvertrag festgelegten Selbstbehalt begrenzt. <br>
-    5.3. Das Fahrzeug ist vollkaskoversichert. Schäden müssen jedoch unverzüglich gemeldet werden. <br>
-    </div>
-    <p> </p>
-
-    <b>6. Stornierung und Änderungen </b> <br>
-
-    <div class="AGBtext">
-    6.1. Stornierungen sind gemäß den im Mietvertrag festgelegten Bedingungen möglich. <br>
-    6.2. Änderungen am Mietvertrag bedürfen der schriftlichen Zustimmung beider Vertragsparteien. <br>
-    </div>
-
-    <p> </p>
-
-    <b> 7. Datenschutz </b> <br>
-
-    <div class="AGBtext">
-    Personenbezogene Daten werden gemäß unserer <a href="http://localhost/Autovermietung/pages/datenschutz.php">Datenschutzerklärung</a> behandelt. <br> <!--href um über den Hyperlink zu Datenschutz zu gelangen-->
-    </div>
-
-    <p> </p>
-
-    <b> 8. Schlussbestimmungen </b> <br>
-
-    <div class="AGBtext">
-    8.1. Es gilt das Recht der Bundesrepublik Deutschland. <br>
-    8.2. Erfüllungsort und Gerichtsstand ist Hamburg. </p>
-    </div>
-
 </div>
 
 <?php
-// Einbinden des Footers
+// Integrating the footer
 include('../includes/footer.html');
 ?>
 
--- pages/impressum.php
+++ pages/impressum.php
@@ -14,10 +14,87 @@
 
 <?php
 
-include('../includes/header.html'); // Einbinden des Headers
+include('../includes/header.html'); // Including the header
 ?>
+
+<div class="ImpressumContainer"> <!--Class to position text in the body -->
+    <h1 class="ImpressumUeberschrift"> Allgemeine Geschäftsbedingungen für SWIFT rentals </h1>
+
+    <b>Verantwortlich für den Inhalt nach § 55 Abs. 2 RStV:  </b> <br>
+
+    SWIFT rentals GmbH <br>
+    
+    <br> <!--To add paragraph-->
+    <b>Kontakt</b> <br>
+    
+    Telefon: 040 123456789 <br>
+    E-Mail: info@swiftrentals.de <br>
+    
+    <br> <!--To add paragraph-->
+
+    <b>Umsatzsteuer-ID</b> <br>
+
+   
+    Umsatzsteuer-Identifikationsnummer gemäß §27a Umsatzsteuergesetz: <br>
+    DE123456789 <br>
+
+   <br> <!--To add paragraph-->
+
+    <b> Register und Registernummer </b> <br>
+
+    Eintragung im Handelsregister. <br>
+    Registergericht: Amtsgericht Hamburg <br>
+    Registernummer: HRB 7890 <br>
+
+    <br> <!--To add paragraph-->
+
+    <b>Vertretungsberechtige:</b> <br>
+    Philipp Rinau (Geschäftsführer) <br>
+
+    <br> <!--To add paragraph-->
+
+    <div class="blocksatz">
+        <b>Haftungsausschluss</b> <br>
+
+            <b>1. Haftung für Inhalte</b> <br>
+            Die Inhalte unserer Seiten wurden mit größter Sorgfalt erstellt. 
+            Für die Richtigkeit, Vollständigkeit und Aktualität der Inhalte können wir jedoch 
+            keine Gewähr übernehmen. Als Diensteanbieter sind wir gemäß § 7 Abs.1 TMG für eigene 
+            Inhalte auf diesen Seiten nach den allgemeinen Gesetzen verantwortlich. Nach §§ 8 bis 10 TMG
+            sind wir als Diensteanbieter jedoch nicht verpflichtet, übermittelte oder gespeicherte fremde
+            Informationen zu überwachen oder nach Umständen zu forschen, die auf eine rechtswidrige
+            Tätigkeit hinweisen. Verpflichtungen zur Entfernung oder Sperrung der Nutzung von
+            Informationen nach den allgemeinen Gesetzen bleiben hiervon unberührt. Eine diesbezügliche
+            Haftung ist jedoch erst ab dem Zeitpunkt der Kenntnis einer konkreten Rechtsverletzung
+            möglich. Bei Bekanntwerden von entsprechenden Rechtsverletzungen werden wir diese Inhalte
+            umgehend entfernen. <br>
+
+            <br> <!--To add paragraph-->
+
+            <b>2. Haftung für Links</b> <br>
+            Unsere Website enthält Links zu externen Websites Dritter, auf deren Inhalte wir keinen
+            Einfluss haben. Für die Inhalte der verlinkten Seiten ist stets der jeweilige Anbieter oder 
+            Betreiber der Seiten verantwortlich. Eine permanente inhaltliche Kontrolle der verlinkten 
+            Seiten ist jedoch ohne konkrete Anhaltspunkte einer Rechtsverletzung nicht zumutbar. 
+            Bei Bekanntwerden von Rechtsverletzungen werden wir derartige Links umgehend entfernen. <br>
+
+            <br> <!--To add paragraph-->
+
+            <b>3. Urheberrecht</b> <br>
+            Die durch die Seitenbetreiber erstellten Inhalte und Werke auf diesen Seiten unterliegen 
+            dem deutschen Urheberrecht. Die Vervielfältigung, Bearbeitung, Verbreitung und jede Art der 
+            Verwertung außerhalb der Grenzen des Urheberrechts bedürfen der schriftlichen Zustimmung des 
+            jeweiligen Autors bzw. Erstellers. Downloads und Kopien dieser Seite sind nur für den 
+            privaten, nicht kommerziellen Gebrauch gestattet. <br>
+
+    </div>
+
+</div>
+
 <?php
-include('../includes/footer.html'); // Einbindung des Footers
+// Integrating the footer
+include('../includes/footer.html');
 ?>
+
 </body>
 </html>
