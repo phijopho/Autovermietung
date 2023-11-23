a42586d205cf41959889bb7207018f9fecf32ae7
clawo <131432761+clawo@users.noreply.github.com>
2023-11-23 13:32:34 +0100

    Produktdetailseite Formatierung vorläufig fertig

--- pages/Produktdetailseite.php
+++ pages/Produktdetailseite.php
@@ -8,45 +8,61 @@
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet"> 
     <!-- Einbinden der style.css -->
-    <link rel="stylesheet" href="/css/style.css">
-    <link rel="stylesheet" href="/css/styleProduktdetailseite.css">
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/styleProduktdetailseite.css">
     <title>Unsere Flotte</title> 
 </head>
  
 <body>
 <?php
 include('/includes/header.html'); // Einbindung des Headers
-
-include('/includes/footer.html'); // Einbindung des Footers
 ?>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<div class="divbody"></div>
-<table>
-        <thead>
-            <tr>
-                <th colspan="2"><h1>Hamburg</h1></th> <!-- colspan="2" kombiniert zwei Zellen zu einer einzigen in der ersten Zeile -->
-            </tr>
-        </thead>
-        <tbody>
-            <tr>
-                <td>Farheugmodell</td>
-                <td>Ihr ausgewählter Zeitraum</td>
-            </tr>
-            <tr>
-                <td><img src="images/cabrio-mercedes-benz-2845333_1920.png" alt="Photo"></td>
-                <td><h3>Fahrzeugtyp: <br> Anzahlsitze: </h3></td>
-            </tr>
-            <!-- Weitere Zeilen können nach Bedarf hinzugefügt werden -->
-        </tbody>
-    </table>
+
+<div class="divbody">
+    <div class="divgallery">
+        <h1>"Farzeugmodell"</h1>
+        <div class="foto">
+  
+        <img src="images/cabrio-mercedes-benz-2845333_1920.png" alt="Auto">
+            <div class="desc">
+                <table>
+                    <tr>
+                        <th>Fahrzeugtyp</th>
+                        <td>"Variable"</td>
+                        <th>Getriebe</th>
+                        <td>"Variable"</td>
+                    </tr>
+                    <tr>
+                        <th>Anzahl Sitze</th>
+                        <td>"Variable"</td>
+                        <th>GPS</th>
+                        <td>"Variable"</td>
+                    </tr>
+                    <tr>
+                        <th>Anzahl Türen</th> 
+                        <td>"Variable"</td>
+                        <th>Klimaanlage</th>
+                        <td>"Variable"</td>    
+                    </tr>
+                </table>
+            </div>
+        </div>
+    </div>
+
+    <div class="divinfo">
+        <h2>Ihr ausgewählter Zeitraum: </h2> <br> 
+            <h2>"Variable Abholdatum" bis "Variable Rückgabedatum" </h2>
+                <h2> Standort des Fahrzeugs: "Varable Standort"</h2>
+                    <h3>Mindestalter: "Variable Alter"</h3>
+        
 
     </div>
+
+
+</div>
+
+<?php    include('/includes/footer.html'); // Einbindung des Footers
+?>
 
 </body>
  
