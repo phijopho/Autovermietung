ff3a788919e4f60f2236db6cfb64ded75173159c
brianna404 <briannakruschke@gmx.de>
2023-11-23 15:34:40 +0100

    english, add functions, add filters

--- includes/functions.php
+++ includes/functions.php
@@ -11,4 +11,34 @@ function getCities(){
     }
     return $location;    
 }
+
+function getCategories(){
+    include('dbConnection.php');
+    $categories=array();
+    $stmtGetCategories = $conn->query("SELECT DISTINCT Type FROM Vendor");
+    while($row = $stmtGetCategories->fetch()){
+        $categories[] = $row['Type'];
+    }
+    return $categories;    
+}
+
+function getVendors(){
+    include('dbConnection.php');
+    $vendors=array();
+    $stmtGetVendors = $conn->query("SELECT Abbreviation FROM Vendor");
+    while($row = $stmtGetVendors->fetch()){
+        $vendors[] = $row['Abbreviation'];
+    }
+    return $vendors;    
+}
+
+function getSeats(){
+    include('dbConnection.php');
+    $seats=array();
+    $stmtGetSeats = $conn->query("SELECT MIN(Seats), MAX(Seats) FROM CarType");
+    $row = $stmtGetSeats->fetch();
+        $seats['min'] = $row['MIN(Seats)'];
+        $seats['max'] = $row['MAX(Seats)'];
+    return $seats;    
+}
 ?>
--- pages/homepage.php
+++ pages/homepage.php
@@ -16,10 +16,10 @@ $location=getCities();
                     }
                     ?>
                 </select>
-            <label for "Abholdatum">Abholdatum:</label>
-                <input type="date" name="Abholdatum" value="<?php echo date('Y-m-d'); ?>" />
-            <label for "Rueckgabedatum">R&uuml;ckgabedatum:</label>
-                <input type="date" name="Rueckgabedatum" value="<?php echo date('Y-m-d'); ?>" /><br><br>
+            <label for "pickUpDate">Abholdatum:</label>
+                <input type="date" name="pickUpDate" value="<?php echo date('Y-m-d'); ?>" />
+            <label for "returnDate">R&uuml;ckgabedatum:</label>
+                <input type="date" name="returnDate" value="<?php echo date('Y-m-d'); ?>" /><br><br>
             <input type="submit" value="Suchen">
         </form>
     </div>
