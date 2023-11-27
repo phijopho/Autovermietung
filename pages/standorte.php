cd197daebf94d4d2252f4669ca089d277b9ca707
philippr1212 <Philipp.Rinau@myhsba.de>
2023-11-27 19:29:10 +0100

    Standorte - Prototyp
    Standorte müssen noch bei der Produktübersichtsseite mit dem jeweiligen Filter "Stadt" verknüpft werden

---
+++ pages/standorte.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Einbinden der style.css -->
+    <link rel="stylesheet" href="../css/style.css"> 
+    <link rel="stylesheet" href="../css/styleStandorte.css">
+    <title>SWIFT rentals</title>
+    <base href="/Autovermietung/">
+
+</head>
+<body>
+
+<?php
+
+include('../includes/header.html'); // Including the header
+?>
+
+
+<iframe width="100%" height="600px" frameborder="0" allowfullscreen allow="geolocation" src="//umap.openstreetmap.fr/de/map/unbenannte-karte_991409?scaleControl=false&miniMap=false&scrollWheelZoom=false&zoomControl=true&editMode=disabled&moreControl=true&searchControl=null&tilelayersControl=null&embedControl=null&datalayersControl=true&onLoadPanel=undefined&captionBar=false&captionMenus=true"></iframe><p><a href="//umap.openstreetmap.fr/de/map/unbenannte-karte_991409?scaleControl=false&miniMap=false&scrollWheelZoom=true&zoomControl=true&editMode=disabled&moreControl=true&searchControl=null&tilelayersControl=null&embedControl=null&datalayersControl=true&onLoadPanel=undefined&captionBar=false&captionMenus=true"></p>
+
+
+
+
+
+<?php
+// Integrating the footer
+include('../includes/footer.html');
+?>
+
+</body>
+</html>
