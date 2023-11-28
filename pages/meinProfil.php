cdc37a5ec64ade76c2ab09a2fb9dbf83aeae82b8
philippr1212 <Philipp.Rinau@myhsba.de>
2023-11-28 17:02:08 +0100

    Mein Profil (unfertig)

--- pages/meinProfil.php
+++ pages/meinProfil.php
@@ -1,17 +1,28 @@
-<!-- Mein Profil Page -->
-<!-- Login Page -->
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <!-- Einbinden der style.css -->
-    <link rel="stylesheet" href="../css/style.css"> 
-    <link rel="stylesheet" href="../css/styleMeinProfil.css">
-    <title>SWIFT rentals</title>
+    <title>Meine Buchungen</title>
     <base href="/Autovermietung/">
-
+    <link rel="stylesheet" href="css/style.css"> 
+    <link rel="stylesheet" href="css/styleMeinProfil.css">
 </head>
 <body>
 
+<?php
+include('../includes/header.html');
+?>
+
+<br>
+<br>
+<br>
+<br>
+
+
+<?php
+include('../includes/footer.html');
+?>
+
 </body>
+</html>
