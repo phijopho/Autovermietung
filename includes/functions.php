f3aa5a8e8a0d2899e83fdc292758d1b5602e2605
clawo <131432761+clawo@users.noreply.github.com>
2023-11-28 13:50:14 +0100

    funktions aktualisiert

--- includes/functions.php
+++ includes/functions.php
@@ -10,6 +10,19 @@ function getMinMaxPrice($Type) {
     return $result; 
 }
 
+function getCities(){
+    include('dbConnection.php');
+    $default="Hamburg";
+    $location = array($default);
+    $stmtGetCities = $conn->prepare("SELECT City FROM Location WHERE City!=:cityIdent");
+    $stmtGetCities->bindParam(':cityIdent', $default);
+    $stmtGetCities->execute();
+    while($row = $stmtGetCities->fetch()){
+        $location[] = $row['City'];
+    }
+    return $location;    
+}
+
 // function showImage($CarType_ID){
 //     include('dbConnection.php');
 //     $image=$conn->prepare("SELECT Image FROM CarType WHERE CarType_ID=:CarTypeIdent");
