2c8e175b2251ef6deb1f9f286a7fecad2bc736c4
brianna404 <briannakruschke@gmx.de>
2023-11-29 09:32:43 +0100

    Code einrücken (Brianna)

--- pages/homepage.php
+++ pages/homepage.php
@@ -1,43 +1,26 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
 <?php 
 include('includes/dbConnection.php');
 include("./includes/functions.php");
 ?>
 
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <!-- Einbinden der style.css -->
-    <link rel="stylesheet" href="../css/style.css"> 
-    <link rel="stylesheet" href="../css/styleHomepage.css">
-    <title>SWIFT rentals</title>
-    <base href="/Autovermietung/">
-
-</head>
-
-
-</head>
 <body>
-
 <div class="BackgroundAudi"> 
     <div class="divContentContainer">
         <!-- <div class="divBookingForm"> -->
-            <div class="containerBookingForm">
-                <!-- <h1>Buchung</h1> -->
-                <h1>Buchung</h1>
-                    <?php 
-                    $pickUpLocation = array("Hamburg");
-                    $default="Hamburg";
-                    $stmtGetCities = $conn->prepare("SELECT City FROM Location WHERE City!=:cityIdent");
-                    $stmtGetCities->bindParam(':cityIdent', $default);
-                    $stmtGetCities->execute();
-                    while($row = $stmtGetCities->fetch()){
-                        $pickUpLocation[] = $row['City'];
-                    }
-                    ?>
-        <!-- Link zur Produktübersichtseite statt index -->
+        <div class="containerBookingForm">
+            <!-- <h1>Buchung</h1> -->
+            <h1>Buchung</h1>
+            <?php 
+            $pickUpLocation = array("Hamburg");
+            $default="Hamburg";
+            $stmtGetCities = $conn->prepare("SELECT City FROM Location WHERE City!=:cityIdent");
+            $stmtGetCities->bindParam(':cityIdent', $default);
+            $stmtGetCities->execute();
+            while($row = $stmtGetCities->fetch()){
+                $pickUpLocation[] = $row['City'];
+            }
+            ?>
+            <!-- Link zur Produktübersichtseite statt index -->
             <form action="./index.php" method="post"> 
                 <label for="Abholort">Abholort:</label>
                     <select id="Abholort" name="Abholort">
@@ -55,63 +38,58 @@ include("./includes/functions.php");
             </form>
         </div>
 
-<div class="divAboutUs">
-  <div class="divGoal1">
-    <p> Every single detail of  SWIFT rentals  is
-        measured against our continuing goal: to
-        enhance costumer enjoyment.
-    </p>
-  </div>
+        <div class="divAboutUs">
+            <div class="divGoal1">
+                <p> Every single detail of  SWIFT rentals  is
+                    measured against our continuing goal: to
+                    enhance costumer enjoyment.
+                </p>
+            </div>
 
-  <div class="divGoal2">
-    <p> Every single detail of  SWIFT rentals  is
-        measured against our continuing goal: to
-        enhance costumer enjoyment.
-    </p>
-  </div>
+            <div class="divGoal2">
+                <p> Every single detail of  SWIFT rentals  is
+                    measured against our continuing goal: to
+                    enhance costumer enjoyment.
+                </p>
+            </div>
 
-  <div class="divGoal3">
-    <p> Every single detail of  SWIFT rentals  is
-        measured against our continuing goal: to
-        enhance costumer enjoyment.
-    </p>
-  </div>
+            <div class="divGoal3">
+                <p> Every single detail of  SWIFT rentals  is
+                    measured against our continuing goal: to
+                    enhance costumer enjoyment.
+                </p>
+            </div>
 
-  <div class="divGoal4">
-    <p> Every single detail of  SWIFT rentals  is
-        measured against our continuing goal: to
-        enhance costumer enjoyment.
-    </p>
-  </div>
-  <br>
-      <div class="divbutton">
-          <a href="http://localhost/Autovermietung/pages/aboutus.php" class="button">Discover more</a>
-      </div>
-</div>
-
-    <div class="divModels">
-
-        <div class="gallery">
-         <?php
-            $type=array("Cabrio", "Combi", "Mehrt&uuml;rer", "SUV", "Coupé", "Limousine");
-            for ($i=1; $i<=6; $i++){
-                       echo "<div class='imageContainer'>";
-                       echo "<img src='images/Default_Car_Cabrio_from_mercedes_no_car_brand_visible_silver_n_0_3ab7f2a6-a473-48dc-ba48-421b05e7453f_0.png' alt='Bild 1'>";
-                       echo "<div class='caption'>";
-                           $MinPrice=getMinMaxPrice($type[$i-1]);
-                       echo $type[$i-1]." ab: ".$MinPrice['min']." &euro;";
-                       echo "</div>";
-                echo "</div>";
-          }
-           
-            ?>
+            <div class="divGoal4">
+                <p> Every single detail of  SWIFT rentals  is
+                    measured against our continuing goal: to
+                    enhance costumer enjoyment.
+                </p>
+            </div>
+            <br>
+            <div class="divbutton">
+                <a href="http://localhost/Autovermietung/pages/aboutus.php" class="button">Discover more</a>
+            </div>
         </div>
 
-    </div> 
-
+        <div class="divModels">
+            <div class="gallery">
+            <?php
+                $type=array("Cabrio", "Combi", "Mehrt&uuml;rer", "SUV", "Coupé", "Limousine");
+                for ($i=1; $i<=6; $i++){
+                    echo "<div class='imageContainer'>";
+                        echo "<img src='images/Default_Car_Cabrio_from_mercedes_no_car_brand_visible_silver_n_0_3ab7f2a6-a473-48dc-ba48-421b05e7453f_0.png' alt='Bild 1'>";
+                        echo "<div class='caption'>";
+                            $MinPrice=getMinMaxPrice($type[$i-1]);
+                            echo $type[$i-1]." ab: ".$MinPrice['min']." &euro;";
+                        echo "</div>";
+                    echo "</div>";
+                }         
+                ?>
+            </div>
+        </div> 
+    </div>
 </div>
-</div>
-
 <br>
 <br>
 <br>
@@ -136,8 +114,6 @@ include("./includes/functions.php");
       });
     });
 
-    
-
     document.addEventListener("DOMContentLoaded", function () {
     const images = document.querySelectorAll(".gallery .imageContainer");
     let currentImageIndex = 0;
@@ -155,14 +131,8 @@ include("./includes/functions.php");
       images[currentImageIndex].style.display = "block";
     }
 
-
     // Add click event listener to show the next image
     document.querySelector('.gallery').addEventListener("click", showNextImage);
   });
 
-
-  </script>
-
-</body>
-
-</html>
+</script>
