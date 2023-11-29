c092f4c68a3665c5ba28cfb07c14e0304e9fdd87
philippr1212 <Philipp.Rinau@myhsba.de>
2023-11-29 18:14:14 +0100

    Buchunsgsseite soweit fertig
    -evt. Farben abändern
    -Footer von article lösen

--- pages/meineBuchungen.php
+++ pages/meineBuchungen.php
@@ -16,8 +16,27 @@
     ?>
 
 <article>
+    <h1>Meine Buchungen</h1>
+
+    <div class="onTopContainer">
+        <h3>Buchungs-ID</h3>
+        <h3>Buchungsdatum</h3>
+        <h3>Abholdatum</h3>
+        <h3>Rückgabedatum</h3>
+        <h3>Hersteller</h3>
+        <h3>Modell</h3>
+    </div>
+
   <dl id="ud_accordion">
-    <dt>Buchungszeile 1</dt>
+    <dt>
+        <h4>2023112901</h4>
+        <h4>29.11.2023</h4>
+        <h4>02.12.2023</h4>
+        <h4>06.12.2023</h4>
+        <h4>Audi</h4>
+        <h4>A8</h4>
+
+    </dt>
     <dd>Platz für weitere Informationen</dd>
   
     <dt>Buchungszeile 2</dt>
@@ -69,6 +88,12 @@ $(document).ready(function() {
     });
 });
 </script>
+
+
+<?php
+    include('../includes/footer.html'); // Einbindung des Footers
+    ?>
+
 
 </body>
 </html>
