09e440b1f4ae7372a8d135792d5511e1a3a2f43f
philippr1212 <Philipp.Rinau@myhsba.de>
2023-11-30 12:08:59 +0100

    code verändert, das aussehen jedoch nicht

--- pages/meineBuchungen.php
+++ pages/meineBuchungen.php
@@ -1,58 +1,56 @@
-<!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <head>
+    <?php
+      include('../includes/htmlhead.php')
+    ?>
     <title>Meine Buchungen</title>
-    <base href="/Autovermietung/">
-    <link rel="stylesheet" href="css/style.css"> 
     <link rel="stylesheet" href="css/styleMeineBuchungen.css">
     <script src="https://code.jquery.com/jquery-3.6.4.min.js"></script>
-</head>
+  </head>
+
 <body>
 
-<?php
+  <?php
     include('../includes/header.html'); // Einbinden des Headers
-    ?>
+  ?>
+    
+  <article>
+      <h1>Meine Buchungen</h1>
 
-<article>
-    <h1>Meine Buchungen</h1>
+      <div class="onTopContainer">
+          <h3>Buchungs-ID</h3>
+          <h3>Buchungsdatum</h3>
+          <h3>Abholdatum</h3>
+          <h3>Rückgabedatum</h3>
+          <h3>Hersteller</h3>
+          <h3>Modell</h3>
+      </div>
 
-    <div class="onTopContainer">
-        <h3>Buchungs-ID</h3>
-        <h3>Buchungsdatum</h3>
-        <h3>Abholdatum</h3>
-        <h3>Rückgabedatum</h3>
-        <h3>Hersteller</h3>
-        <h3>Modell</h3>
-    </div>
+    <dl id="ud_accordion">
+      <dt>
+          <p>2023112901</p>
+          <p>29.11.2023</p>
+          <p>02.12.2023</p>
+          <p>06.12.2023</p>
+          <p>Audi</p>
+          <p>A8</p>
 
-  <dl id="ud_accordion">
-    <dt>
-        <h4>2023112901</h4>
-        <h4>29.11.2023</h4>
-        <h4>02.12.2023</h4>
-        <h4>06.12.2023</h4>
-        <h4>Audi</h4>
-        <h4>A8</h4>
+      </dt>
+      <dd>Platz für weitere Informationen</dd>
+    
+      <dt>Buchungszeile 2</dt>
+      <dd>Platz für weitere Informationen</dd>
 
-    </dt>
-    <dd>Platz für weitere Informationen</dd>
-  
-    <dt>Buchungszeile 2</dt>
-    <dd>Platz für weitere Informationen</dd>
+      <dt>Buchungszeile 3</dt>
+      <dd>Platz für weitere Informationen</dd>
 
-    <dt>Buchungszeile 3</dt>
-    <dd>Platz für weitere Informationen</dd>
+      <dt>Buchungszeile 4</dt>
+      <dd>Platz für weitere Informationen</dd>
 
-    <dt>Buchungszeile 4</dt>
-    <dd>Platz für weitere Informationen</dd>
-
-    <dt>Buchungszeile 5</dt>
-    <dd>Platz für weitere Informationen</dd>
-  </dl>
-</article>
-
+      <dt>Buchungszeile 5</dt>
+      <dd>Platz für weitere Informationen</dd>
+    </dl>
+  </article>
 
 <!--js code for accordion--> 
 <script>
@@ -92,7 +90,7 @@ $(document).ready(function() {
 
 <?php
     include('../includes/footer.html'); // Einbindung des Footers
-    ?>
+?>
 
 
 </body>
--- pages/standorte.php
+++ pages/standorte.php
@@ -18,8 +18,7 @@ include('../includes/header.html'); // Including the header
 ?>
 
 
-<iframe width="100%" height="620px" frameborder="0" allowfullscreen allow="geolocation" src="//umap.openstreetmap.fr/de/map/unbenannte-karte_991409?scaleControl=false&miniMap=false&scrollWheelZoom=false&zoomControl=true&editMode=disabled&moreControl=true&searchControl=null&tilelayersControl=null&embedControl=null&datalayersControl=true&onLoadPanel=undefined&captionBar=false&captionMenus=true"></iframe><span style="display:none" href="//umap.openstreetmap.fr/de/map/unbenannte-karte_991409?scaleControl=false&miniMap=false&scrollWheelZoom=true&zoomControl=true&editMode=disabled&moreControl=true&searchControl=null&tilelayersControl=null&embedControl=null&datalayersControl=true&onLoadPanel=undefined&captionBar=false&captionMenus=true">Vollbildanzeige</span>
-
+<img src="images/Deutschlandkarte.png" class="verkleinertesBild">
 
 <?php
 // Integrating the footer
