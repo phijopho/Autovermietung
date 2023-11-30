1a11f914931f9c7247a61853b2e90d767f1dd484
brianna404 <briannakruschke@gmx.de>
2023-11-30 14:14:23 +0100

    about us html

--- pages/homepage.php
+++ pages/homepage.php
@@ -23,4 +23,42 @@ $location=getCities();
             <input type="submit" value="Suchen">
         </form>
     </div>
+
+    <div class="divAboutUs">
+            <div class="divGoal1">
+                <p> Every single detail of  SWIFT rentals  is
+                    measured against our continuing goal: to
+                    enhance costumer enjoyment.
+                </p>
+            </div>
+
+            <div class="divGoal2">
+                <p> Every single detail of  SWIFT rentals  is
+                    measured against our continuing goal: to
+                    enhance costumer enjoyment.
+                </p>
+            </div>
+
+            <div class="divGoal3">
+                <p> Every single detail of  SWIFT rentals  is
+                    measured against our continuing goal: to
+                    enhance costumer enjoyment.
+                </p>
+            </div>
+
+            <div class="divGoal4">
+                <p> Every single detail of  SWIFT rentals  is
+                    measured against our continuing goal: to
+                    enhance costumer enjoyment.
+                </p>
+            </div>
+            <br>
+            <!-- When click on button, then AboutUs page opens -->
+            <div class="divbutton">
+                <a href="http://localhost/Autovermietung/pages/aboutus.php" class="button">Discover more</a>
+            </div>
+        </div>    
+        
+        
+
 </div>
