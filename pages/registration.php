560f20cb56d26c04a9a275434e21e9f4fc152604
felixvoss <vossfelix2607@gmail.com>
2023-12-03 01:07:23 +0100

    add registration backend

--- pages/registration.php
+++ pages/registration.php
@@ -20,20 +20,60 @@
     <?php
     include('../includes/dbConnection.php');
     include('../includes/header.html'); // Einbinden des Headers
+
+
+    if (isset($_POST["register"])) {
+        var_dump($_POST);
+        $firstName = $_POST["firstName"];
+        $lastName = $_POST["lastName"];
+        $age = $_POST["age"];
+        $email = $_POST["email"];
+        $username = $_POST["username"];
+        $password = password_hash($_POST["password"], PASSWORD_DEFAULT);
+
+        $stmt = $conn->prepare("SELECT * FROM User WHERE Username=:username OR Email=:email");
+        $stmt->bindParam(":username", $username);
+        $stmt->bindParam(":email", $email);
+        $stmt->execute();
+
+        $userExists = $stmt->fetchColumn();
+
+        if (!$userExists) {
+            register($firstName, $lastName, $age, $email, $username, $password);
+        } else {
+            echo "<br> Es existiert bereits ein Account mit diesem Username oder dieser Email.";
+        }
+    }
+
+    function register($firstName, $lastName, $age, $email, $username, $password)
+    {
+        global $conn;
+        $stmt = $conn->prepare("INSERT INTO User (FirstName, LastName, Age, EMail, Username, Password) VALUES (:firstName, :lastName, :age, :email, :username, :password)");
+        $stmt->bindParam(":firstName", $firstName);
+        $stmt->bindParam(":lastName", $lastName);
+        $stmt->bindParam(":age", $age);
+        $stmt->bindParam(":email", $email);
+        $stmt->bindParam(":username", $username);
+        $stmt->bindParam(":password", $password);
+        if ($stmt->execute()) {
+            header("Location: ../index.php");
+        } else { //add include file with error message
+        }
+    }
+
     ?>
 
-    <form>
+    <form action=<?php echo $_SERVER["PHP_SELF"] ?> method="post">
         <h1>Registrieren</h1>
-        <!-- <div class="comment">Mit * gekennzeichnete Felder müssen ausgefüllt werden</div> -->
         <div class="inputbox">
-            <input type="text" required autofocus placeholder="Vorname*">
-            <input type="text" required placeholder="Nachname*">
-            <input type="number" required min="18" max="150" placeholder="Alter*">
-            <input type="email" required placeholder="Email*">
-            <input type="text" required placeholder="Username*">
-            <input type="password" required placeholder="Password*">
+            <input type="text" required autofocus placeholder="Vorname*" name="firstName">
+            <input type="text" required placeholder="Nachname*" name="lastName">
+            <input type="number" required min="18" max="150" placeholder="Alter*" name="age">
+            <input type="email" required placeholder="Email*" name="email">
+            <input type="text" required placeholder="Username*" name="username"> <!-- maybe add pattern to disallow spaces -->
+            <input type="password" required placeholder="Password*" name="password">
         </div>
-        <button>Registrieren</button>
+        <button name="register">Registrieren</button>
     </form>
 
     <?php
