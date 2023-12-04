e5e94300f24df32c42cc3cd24cd3b4f07f9daae5
philippr1212 <Philipp.Rinau@myhsba.de>
2023-12-04 14:28:16 +0100

    php backend in Buchungsübersicht angefangen

--- pages/meineBuchungen.php
+++ pages/meineBuchungen.php
@@ -14,43 +14,70 @@
     include('../includes/header.html'); // Einbinden des Headers
   ?>
     
-  <article>
+    <?php
+    // Annahme: $lastBookingNumber enthält die aktuelle Buchungsnummer aus der Datenbank
+    $lastBookingNumber = 0; // Hier setzt man tatsächlichen Wert aus der Datenbank ein
+
+    // Das aktuelle Datum generieren
+    $currentDate = date("Ymd");
+
+    // Buchungs-ID für erste Buchung formatieren
+    $bookingID = $currentDate . str_pad($lastBookingNumber + 1, 2, '0', STR_PAD_LEFT);
+
+    // Die Buchungsnummer für die zweite Buchung erhöhen
+    $newBookingNumber++;
+
+    // Die Buchungs-ID für die zweite Buchung formatieren
+    $newBookingID = $currentDate . str_pad($newBookingNumber, 2, '0', STR_PAD_LEFT);
+    ?>
+
+
+<!--Buchungsdaten Übersicht-->
+    <article>
       <h1>Meine Buchungen</h1>
 
       <div class="onTopContainer">
-          <h3>Buchungs-ID</h3>
-          <h3>Buchungsdatum</h3>
-          <h3>Abholdatum</h3>
-          <h3>Rückgabedatum</h3>
-          <h3>Hersteller</h3>
-          <h3>Modell</h3>
+        <h3>Buchungs-ID</h3>
+        <h3>Buchungsdatum</h3>
+        <h3>Abholdatum</h3>
+        <h3>Rückgabedatum</h3>
+        <h3>Hersteller</h3>
+        <h3>Modell&nbsp;&nbsp;&nbsp;</h3>
       </div>
 
-    <dl id="ud_accordion">
-      <dt>
-          <p>2023112901</p>
+      <dl id="ud_accordion">
+
+        <!--Buchungszeile 1-->
+        <dt>
+          <p><?php echo $bookingID; ?></p>
           <p>29.11.2023</p>
           <p>02.12.2023</p>
           <p>06.12.2023</p>
           <p>Audi</p>
-          <p>A8</p>
+          <p>Sharan&nbsp;&nbsp;&nbsp;&nbsp;</p>
+        </dt>
 
-      </dt>
-      <dd>Platz für weitere Informationen</dd>
-    
-      <dt>Buchungszeile 2</dt>
-      <dd>Platz für weitere Informationen</dd>
+        <dd>
+          Abhol- und Rückgabeort: <!--Variable--><br>
+          Gesamtpreis der Buchung: <!--Variable--><br>
+        </dd>
 
-      <dt>Buchungszeile 3</dt>
-      <dd>Platz für weitere Informationen</dd>
+        <!--Buchungszeile 2-->
+        <dt>
+          <p><?php echo $newBookingID; ?></p> <!--Wert soll mit jeder neuen Buchung erhöht werden. Das muss ich noch programmieren-->
+          <p>29.11.2023</p>
+          <p>02.12.2023</p>
+          <p>06.12.2023</p>
+          <p>Audi</p>
+          <p>Sharan&nbsp;&nbsp;&nbsp;&nbsp;</p>
+        </dt>
 
-      <dt>Buchungszeile 4</dt>
-      <dd>Platz für weitere Informationen</dd>
-
-      <dt>Buchungszeile 5</dt>
-      <dd>Platz für weitere Informationen</dd>
-    </dl>
-  </article>
+        <dd>
+          Abhol- und Rückgabeort: <!--Variable--><br>
+          Gesamtpreis der Buchung: <!--Variable--><br>
+        </dd>
+      </dl>
+    </article>
 
 <!--js code for accordion--> 
 <script>
