4845d294f3be3da6b1254bf264195f4fd37bee2e
clawo <131432761+clawo@users.noreply.github.com>
2023-12-05 15:39:58 +0100

    Produktdetailseite mit kleinerem body und andere kleine changes

--- pages/Produktdetailseite.php
+++ pages/Produktdetailseite.php
@@ -16,49 +16,56 @@
 <body>
 
 <div class="divbody">
-    <div class="divgallery">
-        <h1>"Fahrzeugmodell"</h1>
-        <div class="foto">
-  
-            <img src="images/Default_Car_Cabrio_from_mercedes_no_car_brand_visible_silver_n_0_3ab7f2a6-a473-48dc-ba48-421b05e7453f_0.png" alt="Auto">
-            <button class="buttonToggle" onclick="togglemenu()">&#9660;</button>
-            <div class="desc" id="desc">
-                <table>
-                    <tr>
-                        <th>Fahrzeugtyp</th>
-                        <td>"Variable"</td>
-                        <th>Getriebe</th>
-                        <td>"Variable"</td>
-                    </tr>
-                    <tr>
-                        <th>Anzahl Sitze</th>
-                        <td>"Variable"</td>
-                        <th>GPS</th>
-                        <td>"Variable"</td>
-                    </tr>
-                    <tr>
-                        <th>Anzahl Türen</th> 
-                        <td>"Variable"</td>
-                        <th>Klimaanlage</th>
-                        <td>"Variable"</td>    
-                    </tr>
-                </table>
+    <div class="divContent">
+        <div class="divgallery">
+            <h1>"Fahrzeugmodell"</h1>
+            <div class="foto">
+    
+                <img src="images/cars/audi-a3-cabrio-rot-offen-2020.png" alt="Auto">
+                <button class="buttonToggle" onclick="togglemenu()">&#9660;</button>
+                <div class="desc" id="desc">
+                    <table>
+                        <tr>
+                            <th>Fahrzeugtyp</th>
+                            <td>"Variable"</td>
+                            <th>Getriebe</th>
+                            <td>"Variable"</td>
+                        </tr>
+                        <tr>
+                            <th>Anzahl Sitze</th>
+                            <td>"Variable"</td>
+                            <th>GPS</th>
+                            <td>"Variable"</td>
+                        </tr>
+                        <tr>
+                            <th>Anzahl Türen</th> 
+                            <td>"Variable"</td>
+                            <th>Klimaanlage</th>
+                            <td>"Variable"</td>    
+                        </tr>
+                    </table>
+                </div>
             </div>
         </div>
-    </div>
-    <div class="divinfo">
-        <div class="divText">
-            <h2> Zusammenfassung </h2><br>
-            <h3>Ihr ausgewählter Zeitraum: </h3>
-            <p>"Variable Abholdatum" bis "Variable Rückgabedatum"</p>
-            <h3> Standort des Fahrzeugs: </h3> <p> "Variable Standort"</p>
-            <h3>Mindestalter: </h3><p> "Variable Alter"</p>
+        <div class="divinfo">
+            <div class="divText">
+                <h2> Zusammenfassung </h2><br>
+                <h3>Ihr ausgewählter Zeitraum: </h3>
+                <p>"Variable Abholdatum" bis "Variable Rückgabedatum"</p>
+                <h3> Standort des Fahrzeugs: </h3> <p> "Variable Standort"</p>
+                <h3>Mindestalter: </h3><p> "Variable Alter"</p>
+                <h3>"Preis"/Tag</h3>
+                <br>
+                <h3>Gesamt: "Preis"</h3>
+
+            </div>
+            </div>
         </div>
         <!-- User is Old enough an signed in. -->
         <div class="divbutton">
-            <a href="#" class="button">Buchen</a>
-        </div>
-    </div>
+                <a href="#" class="button">Jetzt Buchen</a>
+            </div>
+    
 </div>
 
 </body>
