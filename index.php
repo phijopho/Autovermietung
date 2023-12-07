3ae379fa2acc0147c34cf8f1036f8180abb59aac
brianna404 <briannakruschke@gmx.de>
2023-12-07 20:16:21 +0100

    richtige Reihenfolge Karusell

--- index.php
+++ index.php
@@ -64,61 +64,61 @@
             <div class="cslider-carousel">
                 <!-- Einheit 1 -->
                 <div class="cslider-item">
-                    <a href="pages/produktuebersicht.php">
-                        <img src="images/mercedes.png" alt="Slider Image" />
+                <a href="pages/produktuebersicht.php" onclick="submitCategory('Limousine')">                        
+                <img src="images/mercedes.png" alt="Slider Image" />
                         <div class="cslider-text">
                             <h3>Limousine</h3>
-                            <p>Jeder Lord, faehrt einen Ford.</p>
+                            <p>Jeder Lord, f&auml;hrt einen Ford.</p>
                         </div>
                     </a>
                 </div>
                 <!-- Einheit 2 -->
                 <div class="cslider-item">
-                    <a href="images/cars/mb-s-amg-cabrio-2d-weiss-offen-2017.php">
+                    <a href="pages/produktuebersicht.php" onclick="submitCategory('Combi')">                        
                         <img src="images/mercedes.png" alt="Slider Image" />
+                        <div class="cslider-text">
+                            <h3>Combi</h3>
+                            <p>Combi Schlombie, sach ich immer</p>
+                        </div>
+                    </a>
+                </div>
+                <!-- Einheit 3 -->
+                <div class="cslider-item">
+                <a href="pages/produktuebersicht.php" onclick="submitCategory('Cabrio')">                        
+                        <img src="images/cars/vw-touran-van-weiss-2016.png" alt="Slider Image" />
                         <div class="cslider-text">
                             <h3>Cabrio</h3>
                             <p>Für mehr Kopffreiheit...</p>
                         </div>
                     </a>
                 </div>
-                <!-- Einheit 3 -->
-                <div class="cslider-item">
-                    <a href="pages/produktuebersicht.php">
-                        <img src="images/cars/vw-touran-van-weiss-2016.png" alt="Slider Image" />
-                        <div class="cslider-text">
-                            <h3>Mehrsitzer</h3>
-                            <p>Für ihre Kinder</p>
-                        </div>
-                    </a>
-                </div>
                 <!-- Einheit 4 -->
                 <div class="cslider-item">
-                    <a href="pages/produktuebersicht.php">
+                <a href="pages/produktuebersicht.php" onclick="submitCategory('SUV')">                        
                         <img src="images/mercedes.png" alt="Slider Image" />
-                        <div class="cslider-text">
-                            <h3>Coupé</h3>
-                            <p>Für die Autobahn</p>
-                        </div>
-                    </a>
-                </div>
-                <!-- Einheit 5 -->
-                <div class="cslider-item">
-                    <a href="pages/produktuebersicht.php">
-                        <img src="images/cars/bmw-x4-m40i-5d-schwarz-2019-1.png" alt="Slider Image" />
                         <div class="cslider-text">
                             <h3>SUV</h3>
                             <p>Fuer alle, die auf Off-Road in der Stadt nicht verzichten koennen.</p>
                         </div>
                     </a>
                 </div>
+                <!-- Einheit 5 -->
+                <div class="cslider-item">
+                <a href="pages/produktuebersicht.php" onclick="submitCategory('Mehrsitzer')">                        
+                        <img src="images/cars/bmw-x4-m40i-5d-schwarz-2019-1.png" alt="Slider Image" />
+                        <div class="cslider-text">
+                            <h3>Mehrsitzer</h3>
+                            <p>Für Ihre Kinder</p>
+                        </div>
+                    </a>
+                </div>
                 <!-- Einheit 6 -->
                 <div class="cslider-item">
-                    <a href="pages/produktuebersicht.php">
+                <a href="pages/produktuebersicht.php" onclick="submitCategory('Coupe')">                        
                         <img src="images/mercedes.png" alt="Slider Image" />
                         <div class="cslider-text">
-                            <h3>Kombi</h3>
-                            <p>Kombi Schlombie, sach ich immer</p>
+                            <h3>Coup&eacute;</h3>
+                            <p>Für die Autobahn</p>
                         </div>
                     </a>
                 </div>
