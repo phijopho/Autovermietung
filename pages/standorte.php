0a41ec0b53a619ead2377610373230a3ad4f4c7e
philippr1212 <Philipp.Rinau@myhsba.de>
2023-12-08 15:44:23 +0100

    includes für Verknüpfung

--- pages/standorte.php
+++ pages/standorte.php
@@ -9,6 +9,11 @@
     <title>Standorte</title>
     <base href="/Autovermietung/">
 
+    <?php
+        include('../includes/htmlhead.php'); // include head
+        include('../includes/dbConnection.php'); // connect database
+        include('../includes/functions.php'); // get functions
+    ?>
 </head>
 <body>
 
