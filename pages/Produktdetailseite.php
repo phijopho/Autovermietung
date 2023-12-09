28b9a5c8d103b6a25a0f0c2a0f5b4720461d3196
brianna404 <briannakruschke@gmx.de>
2023-12-09 20:59:21 +0100

    add produktdetailseite from clara manually

--- pages/Produktdetailseite.php
+++ pages/Produktdetailseite.php
@@ -1,54 +1,53 @@
 <?php 
-session_start(); 
-include('../includes/functions.php');
-// show error messages
- error_reporting(E_ALL);
- ini_set('display_errors', 1);
+    session_start(); 
+    include('../includes/functions.php');
+    // show error messages
+    error_reporting(E_ALL);
+    ini_set('display_errors', 1);
 
-if(isset($_GET['carType_ID'])) {
-    // CarType ID
-    $_SESSION['carType_ID']=$_GET['carType_ID'];
-    // Availabe Cars of that type
-    $stmt=getAvailableCarsForModelQuery($_SESSION['carType_ID']);
-    $_SESSION['availableCarsModel']=getAvailableCarsForModel($stmt);
-} else {
-    echo "Ungültige Abfrage";
-}
+    if(isset($_GET['carType_ID'])) {
+        // CarType ID
+        $_SESSION['carType_ID']=$_GET['carType_ID'];
+        // Availabe Cars of that type
+        $stmt=getAvailableCarsForModelQuery($_SESSION['carType_ID']);
+        $_SESSION['availableCarsModel']=getAvailableCarsForModel($stmt);
+    } else {
+        echo "Ungültige Abfrage";
+    }
 
-// checks
-echo "CarType_ID from Session: ".$_SESSION['carType_ID'];
-echo "<br>Available Cars for this model from Session: ".$_SESSION['availableCarsModel'];
+    // checks
+    echo "CarType_ID from Session: ".$_SESSION['carType_ID'];
+    echo "<br>Available Cars for this model from Session: ".$_SESSION['availableCarsModel'];
 ?>
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-     <!-- Einbinden der Schriftart -->
-     <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet"> 
-    <!-- Einbinden der style.css -->
-    <base href="/Autovermietung/">
-    <link rel="stylesheet" href="css/style.css">
+    <?php
+        include('../includes/htmlhead.php')
+    ?>
     <link rel="stylesheet" href="css/styleProduktdetailseite.css">
-    <title>Unsere Flotte</title> 
-    
+    <script src="includes/functions.js"></script> 
+    <title>Produktdetails</title> 
 </head>
- 
-<body>
+<header>
 <?php
-include('../includes/header.html'); // Einbindung des Headers
+    include('../includes/header.html'); // Einbindung des Headers
 ?>
+</header> 
+
+<body>
 
 <div class="divbody">
     <div class="divgallery">
-        <h1>"Fahrzeugmodell"</h1>   
+        <h1>"Fahrzeugmodell"</h1>
         <div class="foto">
-  
-        <img src="images/cabrio-mercedes-benz-2845333_1920.png" alt="Auto">
-            <div class="desc">
+
+            <img src="images/cars/audi-a3-cabrio-rot-offen-2020.png" alt="Auto">
+            
+            <!-- create button with triangle -->
+            <button class="buttonToggle" onclick="togglemenu()">&#9660;</button>
+        
+            <div class="desc" id="desc">
                 <table>
                     <tr>
                         <th>Fahrzeugtyp</th>
@@ -69,42 +68,35 @@ include('../includes/header.html'); // Einbindung des Headers
                         <td>"Variable"</td>    
                     </tr>
                 </table>
+                </div>
             </div>
+    </div>
+    <div class="divinfo">
+        <div class="divText">
+            <h2> Zusammenfassung </h2><br>
+            <h3>Ihr ausgewählter Zeitraum: </h3>
+            <p>"Variable Abholdatum" bis "Variable Rückgabedatum"</p>
+            <h3> Standort des Fahrzeugs: </h3> <p> "Variable Standort"</p>
+            <h3>Mindestalter: </h3><p> "Variable Alter"</p>
+            <h3>"Preis"/Tag</h3>
+            <br>
+            <h3>Gesamt: "Preis"</h3>
+
         </div>
     </div>
-
-    <div class="divinfo">
-        <h2>Ihr ausgewählter Zeitraum: </h2> <br> 
-            <h2>"Variable Abholdatum" bis "Variable Rückgabedatum" </h2>
-                <h2> Standort des Fahrzeugs: "Variable Standort"</h2>
-                    <h3>Mindestalter: "Variable Alter"</h3>
-
-                    <!-- User is Old enough an signed in. -->
-                        <div class="divbutton">
-                            <a href="#" class="button">Buchen</a>
-                        </div>
-
-                    <!-- If User is not Old enough Errormessage appears -->
-
-                    <!-- <div class="divbutton">
-                        <a href="#" class="buttonNotOldEnough">Buchen</a>
-                    </div> -->
-
-                    <!-- If User not signed in, link to login page -->
-
-                    <!-- <div class="divbutton">
-                        <a href="#" class="buttonNotSignedIn">Bitte Anmelden</a>
-                    </div> -->
+    
+    <!-- User is Old enough and signed in. -->
+    <div class="divbutton">
+            <a href="#" class="buttonNotSignedIn">Jetzt Buchen</a>
     </div>
-
-
 </div>
 
-<?php    include('../includes/footer.html'); // Einbindung des Footers
-?>
-
 </body>
- 
+
+<footer>
+<?php 
+    include('../includes/footer.html'); // Einbindung des Footers
+?>
+</footer>
 </html>
- 
  
