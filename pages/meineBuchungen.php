24112215aa97516817aeb3344c8c610748cd91b9
philippr1212 <Philipp.Rinau@myhsba.de>
2023-12-12 21:19:02 +0100

    pagination added (unfertig)

--- pages/meineBuchungen.php
+++ pages/meineBuchungen.php
@@ -31,6 +31,7 @@
     <h3>Modell&nbsp;&nbsp;&nbsp;</h3>
   </div>
 
+
   <?php 
   // retrieve Infos from database to fill them into accordion
     $userID=getUserID();
@@ -61,6 +62,18 @@
       }
       ?>
   </dl>
+
+  <div class="pagination">
+  <a href="#">&laquo;</a>
+  <a href="#">1</a>
+  <a href="#" class="active">2</a>
+  <a href="#">3</a>
+  <a href="#">4</a>
+  <a href="#">5</a>
+  <a href="#">6</a>
+  <a href="#">&raquo;</a>
+</div>
+
 </article>
 
 <?php
