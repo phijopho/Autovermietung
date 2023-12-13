cf743d1907d65d15266be64568b0ec016b36f9ef
philippr1212 <Philipp.Rinau@myhsba.de>
2023-12-13 14:49:18 +0100

    added Bestätigungsnachricht in Kontaktformular

--- pages/Kontaktformular.php
+++ pages/Kontaktformular.php
@@ -23,38 +23,12 @@
 <?php
 include('../includes/dbConnection.php');
 
-$confirmationMessage = ''; // Neue Variable für die Bestätigungsnachricht
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    try {
-        // Formulardaten abrufen, nachdem das Formular abgeschickt wurde 
-        $vorname = $_POST['vorname'];
-        $nachname = $_POST['nachname'];
-        $email = $_POST['e-mail'];
-        $nachricht = $_POST['Nachricht'];
-
-        // Prepared Statements funktionieren noch nicht! Müssen dringend gefixed werden
-            // $stmt = $dbh->prepare("INSERT INTO anfragen (vorname, nachname, email, nachricht) VALUES (:vorname, :nachname, :email, :nachricht)");
-            //$stmt->bindParam(':vorname', $vorname, PDO::PARAM_STR);
-            //$stmt->bindParam(':nachname', $nachname, PDO::PARAM_STR);
-            //$stmt->bindParam(':email', $email, PDO::PARAM_STR);
-            //$stmt->bindParam(':nachricht', $nachricht, PDO::PARAM_STR);
-
-            // SQL-Befehl ausführen
-            //$stmt->execute();
-
-        // Bestätigungsnachricht anzeigen
-              $confirmationMessage = sprintf('Vielen Dank für Ihre Anfrage! Ihre Daten wurden gespeichert.', htmlspecialchars($vorname));
-          } catch (PDOException $e) {
-              echo "Error: " . $e->getMessage();
-          }
+    // Display confirmation message
+    echo '<script>alert("Vielen Dank für Ihre Anfrage! Wir werden uns bald mit Ihnen in Verbindung setzen.");</script>';
 }
 ?>
-
-<!-- Bestätigungsnachricht nur anzeigen, wenn sie gesetzt ist und wenn der Button geklickt wurde -->
-<?php if (!empty($confirmationMessage) && isset($_POST['login'])) : ?>
-    <div id="confirmation"><?php echo $confirmationMessage; ?></div>
-<?php endif; ?>
 
 
 <form action="<?php echo $_SERVER['PHP_SELF']; ?>" id="ft-form" method="POST" accept-charset="UTF-8">
