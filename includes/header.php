c081f98eac2025a727d517e2cab64ffb81fed473
clawo <131432761+clawo@users.noreply.github.com>
2023-12-13 12:45:09 +0100

    hobermenü fix

--- includes/header.php
+++ includes/header.php
@@ -1,19 +1,17 @@
-<?php session_start(); ?>
 <div class="headercontainer">
     <div class="logobox"><a href="index.php"><img src="images/SWIFT.svg" alt="SWIFT logo"></a></div>
     <div class="headerbox"><a class="nav-link" href="./pages/produktuebersicht.php">Auto mieten</a></div>
-    <div class="headerbox"><a class="nav-link scroll-link" data-target="section2" href="index.php#section2">Preise</a></div>
-    <div class="headerbox"><a class="nav-link scroll-link" data-target="map-container" href="index.php#map-container" >Standorte</a></div>
+    <div class="headerbox"><a class="nav-link scroll-link" data-target="section2" href="#">Preise</a></div>
+    <div class="headerbox"><a class="nav-link scroll-link" data-target="map-container" href="#map-container" >Standorte</a></div>
     <?php if (isset($_SESSION["firstName"]) && !empty($_SESSION["firstName"])) { ?>
-
-      <div class="divhover nav-link" onmouseleave="handleMouseLeave(event)">
-    <ul>
-        <li onmouseenter="handleMouseEnter()">
+        <div class="divhover">
+        <ul>
+            <li>
                 <a>Hallo <?php echo $_SESSION["firstName"] ?> </a>
-              <ul id="submenu" onmouseenter="cancelCloseTimer()" onmouseleave="startCloseTimer()">
-              <li class="nav-link"><a href="#"><p>Mein Profil</p></a></li>
-                <li class="nav-link"><a href="#"><p>Meine Buchung</p></a></li>
-                <li class="nav-link" style="border-bottom-left-radius: 20px; border-bottom-right-radius: 20px;"><a href="./includes/logout.php">Abmelden</a></li>
+              <ul>
+                <li><a href="#">Mein Profil</a></li>
+                <li><a href="pages/meineBuchungen.php">Meine Buchung</a></li>
+                <li style="border-bottom-left-radius: 20px; border-bottom-right-radius: 20px;"><a href="./includes/logout.php">Abmelden</a></li>
               </ul>
             </li>
           </ul>
