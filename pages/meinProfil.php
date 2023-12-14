02d22a2cb8274aebccc0739a08fd87ade2d6e35d
philippr1212 <Philipp.Rinau@myhsba.de>
2023-12-14 13:22:30 +0100

    Mein Profil

--- pages/meinProfil.php
+++ pages/meinProfil.php
@@ -6,13 +6,10 @@
       include('../includes/htmlhead.php')
     ?>
 
-    <?php
-        include('dbConnection.php');
-    ?>
-
     <!-- html page specifics -->
-    <link rel="stylesheet" href="css/styleMeinProfil.css">
+    <link rel="stylesheet" href="css/styleRegistration.css">
     <link rel="stylesheet" href="css/styleFooter.css">
+
     <title>Mein Profil</title>
 </head>
 
@@ -20,22 +17,21 @@
     include('../includes/header.php'); // Einbinden des Headers
 ?>
 <body>
+    <!--Benutzerdaten abrufen und in $user speichern-->
+    <?php
+        $userID = $_SESSION['User_ID'];
 
-<!--Benutzerdaten abrufen und in $user speichern-->
-<?php
-$userID = $_SESSION['User_ID'];
+        // Benutzerdaten abrufen und in $user speichern
+        $query = $conn->prepare("SELECT * FROM user WHERE User_ID = :userID");
+        $query->bindParam(':userID', $userID);
+        $query->execute();
 
-// Benutzerdaten abrufen und in $user speichern
-$query = $conn->prepare("SELECT * FROM user WHERE User_ID = :userID");
-$query->bindParam(':userID', $userID);
-$query->execute();
+        // Holen der Ergebnisse als assoziatives Array
+        $user = $query->fetch(PDO::FETCH_ASSOC);
 
-// Holen der Ergebnisse als assoziatives Array
-$user = $query->fetch(PDO::FETCH_ASSOC);
-
-// Datenbankverbindung schließen
-$conn = null;
-?>
+        // Datenbankverbindung schließen
+        $conn = null;
+    ?>
 
 
 <div class="contentBox">
@@ -45,29 +41,19 @@ $conn = null;
     
     <form>
         <h1>Mein Profil</h1>
-        <div class="inputbox">
-            <input type="text" required placeholder="Vorname" value="<?php echo $user['FirstName']; ?>">
-            <input type="text" required placeholder="Nachname" value="<?php echo $user['LastName']; ?>">
-            <input type="text" required placeholder="Alter" value="<?php echo $user['Age']; ?>">
-            <input type="email" required placeholder="Email" value="<?php echo $user['EMail']; ?>">
-            <input type= "text" required placeholder="Username" value="<?php echo $user['Username']; ?>">
-            <input type="text" required placeholder="Password" value="<?php echo $user['Password']; ?>">
-        </div>
-        <button>Bearbeiten</button>
-    </form>
-</div>
+            <div class="inputbox">
+                <input type="text" required placeholder="Vorname" value="<?php echo $user['FirstName']; ?>">
+                <input type="text" required placeholder="Nachname" value="<?php echo $user['LastName']; ?>">
+                <input type="text" required placeholder="Alter" value="<?php echo $user['Age']; ?>">
+                <input type="email" required placeholder="Email" value="<?php echo $user['EMail']; ?>">
+                <input type= "text" required placeholder="Username" value="<?php echo $user['Username']; ?>">
+                <input type="text" required placeholder="Password" value="<?php echo $user['Password']; ?>">
+                
+                <button type="submit">Bearbeiten</button>
+          </div>
 
 
+   
 </body>
 
-<?php
-    include('../includes/footer.html');
-?>
-
 </html>
-
-
-
-
-
-
