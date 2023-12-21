91415b9a6db461a870b84322376437606be8da04
felixvoss <vossfelix2607@gmail.com>
2023-12-21 21:28:10 +0100

    add comnments for logout

--- includes/logout.php
+++ includes/logout.php
@@ -7,12 +7,12 @@
 <body>
     <?php
     session_start();
-        if (isset($_SESSION["firstName"]) && !empty($_SESSION["firstName"])) {
+        if (isset($_SESSION["firstName"]) && !empty($_SESSION["firstName"])) { // check if user is logged in
             session_unset();
             session_destroy();
-            header("Location: ../index.php?loggedOut=1");
+            header("Location: ../index.php?loggedOut=1"); // redirect user to homepage and display successful logout message
         } else {
-            header("Location: ../index.php");
+            header("Location: ../index.php"); // if not logged in, redirect user to homepage without logging out or displaying any message
         }
     ?>
 </body>
