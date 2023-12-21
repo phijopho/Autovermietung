983889fa2f21cf5045a1b765bc9cf998ad16a9c3
felixvoss <vossfelix2607@gmail.com>
2023-12-21 22:12:22 +0100

    add comments functionRegister

--- includes/functionsRegister.php
+++ includes/functionsRegister.php
@@ -2,33 +2,36 @@
 
 function register()
 {
-    if (isset($_POST["register"])) {
-        $firstName = $_POST["firstName"];
+    if (isset($_POST["register"])) { // check if register button was clicked
+        $firstName = $_POST["firstName"]; // save input values from form into variables
         $lastName = $_POST["lastName"];
         $age = $_POST["age"];
         $email = $_POST["email"];
         $username = $_POST["username"];
-        $password = password_hash($_POST["password"], PASSWORD_DEFAULT);
+        $password = password_hash($_POST["password"], PASSWORD_DEFAULT); // hash password
 
-        $userExists = userExists($username, $email);
+        $userExists = userExists($username, $email); // save return value of checking if user exists in variable
 
         handleRegistration($userExists, $firstName, $lastName, $age, $email, $username, $password);
     }
 }
 
+// check if user is already registered
 function userExists($username, $email)
 {
+    // search database with prepared statement to see if submitted username or email from form is already in database
     global $conn;
     $stmt = $conn->prepare("SELECT * FROM User WHERE Username=:username OR Email=:email");
     $stmt->bindParam(":username", $username);
     $stmt->bindParam(":email", $email);
     $stmt->execute();
-    return $stmt->fetchColumn();
+    return $stmt->fetchColumn(); // return value of next column in database if found (true). otherwise, meaning user isnt registered, return false
 }
 
+// registration handler
 function handleRegistration($userExists, $firstName, $lastName, $age, $email, $username, $password)
 {
-    if (!$userExists) {
+    if (!$userExists) { // check if user is not registered yet
         addUserToDatabase($firstName, $lastName, $age, $email, $username, $password);
         displaySuccessMessage();
     } else {
@@ -36,6 +39,7 @@ function handleRegistration($userExists, $firstName, $lastName, $age, $email, $u
     }
 }
 
+// add user to database with prepared statement and using values of submitted form
 function addUserToDatabase($firstName, $lastName, $age, $email, $username, $password)
 {
     global $conn;
